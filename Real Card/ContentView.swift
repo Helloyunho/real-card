38803ec6712c74d8281ecf8c49d9b9f7c75c8ab7
Helloyunho <yunho050840@gmail.com>
2022-09-14 20:47:10 +0900

    Initial Commit

---
+++ Real Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Real Card
+//
+//  Created by Helloyunho on 2022/09/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Real Card/Real_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_CardApp.swift
+//  Real Card
+//
+//  Created by Helloyunho on 2022/09/14.
+//
+
+import SwiftUI
+
+@main
+struct Real_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
